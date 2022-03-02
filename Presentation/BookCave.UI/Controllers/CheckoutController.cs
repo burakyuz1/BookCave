5fe318525bced801499bb360261f671e168cadd6
Burak Akyüz <burak.akyuz@bilgeadamboost.com>
2022-03-02 21:35:48 +0300

    init

---
+++ Presentation/BookCave.UI/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCave.UI.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation/BookCave.UI/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCave.UI.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
