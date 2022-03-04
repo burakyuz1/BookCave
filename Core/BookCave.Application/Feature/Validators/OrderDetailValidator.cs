7a156d11b6b8df49b46d0d2c7f2f1c7dbdf550e8
ezgiyildirim21 <ezgiyildirim38@gmail.com>
2022-03-04 12:46:24 +0300

    addedfluentvalidation

---
+++ Core/BookCave.Application/Feature/Validators/OrderDetailValidator.cs
@@ -0,0 +1,30 @@
+﻿using BookCave.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCave.Application.Feature.Validators
+{
+    class OrderDetailValidator : AbstractValidator<OrderDetail>
+    {
+        private readonly static int maxLength = 50;
+        private readonly string nullOrEmptyMessage = "This field is required";
+        private readonly string maxLengthMessage = $"This field can be maximum {maxLength} characters";
+
+        public OrderDetailValidator()
+        {            
+            RuleFor(x => x.UnitPrice)
+                .NotEmpty().WithMessage(nullOrEmptyMessage)
+                .NotNull().WithMessage(nullOrEmptyMessage)
+                .InclusiveBetween(0, 255).WithMessage($"Quantity can be between 0 and 255");
+
+            RuleFor(x => x.Quantity)
+                .NotEmpty().WithMessage(nullOrEmptyMessage)
+                .NotNull().WithMessage(nullOrEmptyMessage);
+
+        }
+    }
+}
