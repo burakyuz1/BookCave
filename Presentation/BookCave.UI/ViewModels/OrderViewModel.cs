ef61df0d0c1e24e0e1c501450b1df3da16b705b5
AysegulCelk <gulsadress@gmail.com>
2022-03-21 17:53:13 +0300

    home-shop-card-edited

--- Presentation/BookCave.UI/ViewModels/OrderViewModel.cs
+++ Presentation/BookCave.UI/ViewModels/OrderViewModel.cs
@@ -4,19 +4,19 @@ namespace BookCave.UI.ViewModels
 {
     public class OrderViewModel
     {
-        [Required(ErrorMessage ="Hata var")]
-        public string Name { get; set;}
-        [Required(ErrorMessage = "Hata var")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Hata var")]
+        [Required(ErrorMessage = "This field is required")]
         public string Address { get; set; }
 
-        [Required(ErrorMessage = "Hata var")]
+        [Required(ErrorMessage = "This field is required")]
         public string City { get; set; }
-        [Required(ErrorMessage = "Hata var")]
+        [Required(ErrorMessage = "This field is required")]
         public string Country { get; set; }
-        [Required(ErrorMessage = "Hata var")]
+        [Required(ErrorMessage = "This field is required")]
         [Phone]
         public string Phone { get; set; }
     }
