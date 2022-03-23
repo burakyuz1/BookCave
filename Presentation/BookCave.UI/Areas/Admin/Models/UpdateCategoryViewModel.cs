bc0d2e6181a4f75f64aef29aed6db3b5100fc0c3
ezgiyildirim21 <ezgiyildirim38@gmail.com>
2022-03-23 16:38:56 +0300

    add category and order to admin panel

---
+++ Presentation/BookCave.UI/Areas/Admin/Models/UpdateCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCave.UI.Areas.Admin.Models
+{
+    public class UpdateCategoryViewModel
+    {
+        [Required(ErrorMessage = "Can not pass empty!")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Can not pass empty!")]
+        [MaxLength(50, ErrorMessage = "Only 50 characters")]
+        public string CategoryName { get; set; }
+    }
+}
